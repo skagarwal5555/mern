133e81d757d9744b09ee9e0a1f0bf3f6cc59449c
Sourav Agarwal <agarwal.sourav@tcs.com>
2022-07-15 17:28:03 +0000

    Add Step UI Change

--- front-end/redux/todolist/src/App.js
+++ front-end/redux/todolist/src/App.js
@@ -4,7 +4,6 @@ import Counter from "./components/Counter";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
       <Counter></Counter>
     </div>
   );
--- front-end/redux/todolist/src/components/Counter.js
+++ front-end/redux/todolist/src/components/Counter.js
@@ -9,10 +9,11 @@ function Counter() {
   return (
     <div>
       <h3>Counter</h3>
-      <h3>{counter}</h3>
-      <button onClick={() => dispatch(increment())}>Increase</button>
-      <button onClick={() => dispatch(reset())}>Reset</button>
-      <button onClick={() => dispatch(decrement())}>Decrease</button>
+      <h3>You have walked {counter} steps today</h3>
+      <button onClick={() => dispatch(increment())}>Add a Step</button>
+      <br></br>
+      <br></br>
+      <button onClick={() => dispatch(reset())}>Reset Steps</button>
     </div>
   );
 }
